ef1a29331bc2dbb5bf89aba48ad90e94dc963549
Horikawa Family <horikawa@HorikawanoiMac.local>
2026-03-20 14:51:06 +0900

    feat: Google Analytics追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import "../styles/globals.css";
 
+const GA_ID = "G-GEGZ1ZVRZL";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
